91d77bd8194b6deef5e676713046dc1e243cfb4b
Stephen Adams <tsadams@gmail.com>
2017-12-18 08:37:42 -0500

    Modify code to work with new donation site
    - Modify URLs for donations and streamlabs API
    - Modify donation amount
    - Modify regular expression to search resulting table on donation page
    - Add donation alert to track donations in donation ticker via streamlabs
    - Add absolute path to text files to avoid 'file not found' errors

--- parser.py
+++ parser.py
@@ -13,18 +13,29 @@ from selenium import webdriver
 import time
 import requests
 import os
+import uuid
 
 #Twitchalerts API
-url = 'http://www.twitchalerts.com/api/v1.0/alerts'
+url = 'https://streamlabs.com/api/v1.0/alerts'
+
+#Donate API
+durl = 'https://streamlabs.com/api/v1.0/donations'
 
 # Payload. Use the subscription alert function. Add the sound and gif to that.
 # Used subscription because our particular channel isn't partnered and this
 # alert will never fire unless it's a donation from toys for tots
 payload = {
-    "access_token": "API_ACCESS_TOKEN",
-    "duration": "5",
+    "access_token": "ACCESS_TOKEN",
+    "duration": "12000",
     "type": "subscription",
     "special_text_color": "#ff0000",
+    "sound_href": "https://uploads.twitchalerts.com/000/108/569/223/TwitchAlertAudio.ogg",
+    "image_href": "https://uploads.twitchalerts.com/000/108/569/223/Make%20it%20rain.gif",
+}
+
+query = {
+    "access_token": "ACCESS_TOKEN",
+    "currency": "USD",
 }
 
 # Define the alert function that will be used to actually fire to streamlabs
@@ -32,6 +43,13 @@ def alert(name, donation):
     payload['message'] = '*' + str(name) + '* has donated *' + str(donation) + '* to Toys For Tots!'
     session = requests.Session()
     response = session.post(url, data=payload)
+
+def donate(name, donation):
+    query['name'] = name.replace(' ', '_')
+    query['identifier'] = str(uuid.uuid4())
+    query['amount'] = donation.strip('$')
+    session = requests.Session()
+    response = session.post(durl, data=query)
 
 def get_driver():
     cwd = os.path.dirname(os.path.realpath(__file__))
@@ -43,14 +61,14 @@ def get_driver():
     return driver
 
 driver = get_driver()
-driver.get("https://p2p.charityengine.net/ToysforTotsFoundation/Dashboard/Donations/") # go to the url
+driver.get("URL containing table of donations") # go to the url
 
 # log in
-username_field = driver.find_element_by_name('UserName') # get the username field
-password_field = driver.find_element_by_name('Password') # get the password field
+username_field = driver.find_element_by_name('user[email]') # get the username field
+password_field = driver.find_element_by_name('user[password]') # get the password field
 username_field.send_keys("username") # enter in your username
 password_field.send_keys("password") # enter in your password
-password_field.submit() # submit it
+driver.find_element_by_id('new_user').submit()
 
 # gotta wait while the new site loads
 time.sleep(3)
@@ -71,28 +89,28 @@ r = redis.StrictRedis(host='172.17.0.2', port=6379)
 # create a result that will hold each dictionary
 result = []
 
+split = str.splitlines(html)
 # Loop over the lines in the HTML to get the lines we want
-for line in str.splitlines(html):
+for line in split:
+    match = re.search('\s*<td id="donor_name\d+">', line)
+    money = re.search('\s*<td id="donation_amount\d+" class="text-center">', line)
     # Find the username line in the table
     if re.search("(<td>Opted out of communications<\/td>)", line):
         continue
-    if re.search("(<td>([0-9a-zA-Z'*-*]+\s*)*<\/td>)", line):
+    if match:
         # Create a hold dictionary when we find a name
         hold = {}
-        line = re.sub('<\/*td>', '', line)
-        hold['name'] = str(line).strip()
-    # Find the donation amount. It will be the next lien so we should be good
-    if re.search("<td>\$[0-9]+\.[0-9]{2}<\/td>", line):
-        line = re.sub('<\/*td>', '', line)
-        hold['donation'] = str(line).strip()
+        hold['name'] = split[split.index(match.group(0)) + 1].strip()
+    # Find the donation amount. It will be the next line so we should be good
+    if money:
+        hold['donation'] = split[split.index(money.group(0)) + 1].strip()
         # Append the hold dictionary to result so we have something like this:
         # {'donation': '$10.00', 'name':'SteveHNH'}
         result.append(hold)
 
-
 # open a file for writing in append mode. This adds lines to the bottom of the
 # file, rather than overwriting it.
-f = open('./html/donations.txt', 'a+')
+f = open('/root/Twitch-Alert-System-T4T/html/donations.txt', 'a+')
 
 # iterate over the result list, load each dict into the redis db as a keypair
 # and write the data set to donations.txt for use in a webservice
@@ -106,28 +124,27 @@ for i in result:
         time.sleep(15)
         # Pull the name and donation from the dictionary, not redis.
         alert(i.get('name'), i.get('donation'))
+        donate(i.get('name'), i.get('donation'))
 f.close()
 
-with open('./html/donations.txt', 'r') as content_file:
+with open('/root/Twitch-Alert-System-T4T/html/donations.txt', 'r') as content_file:
     content = content_file.read()
 
-f = open('./html/total.txt', 'w+')
+f = open('/root/Twitch-Alert-System-T4T/html/total.txt', 'w+')
 total = 0
 for line in content.splitlines():
-   dollar = re.search("[0-9]+\.[0-9]{2}", line)
-   dollar = dollar.group()
-   total = total + float(dollar)
+    dollar = re.search("[0-9]+\.[0-9]{2}", line)
+    dollar = dollar.group()
+    total = total + float(dollar)
 f.write('${:,.2f}'.format(total) + '\n')
 f.close()
 
 # write the percentage to a file
-f = open('./html/percent.txt', 'w+')
-percentage = float(total) / 500
+f = open('/root/Twitch-Alert-System-T4T/html/percent.txt', 'w+')
+percentage = float(total) / 1000
 f.write("{:.0%}".format(percentage) + '   \n')
-f.close()
-
 # Get the number of lines so we can feed it to the meter
-num_lines = sum(1 for line in open('./html/donations.txt'))
-f = open('./html/lines.txt', 'w+')
+num_lines = sum(1 for line in open('/root/Twitch-Alert-System-T4T/html/donations.txt'))
+f = open('/root/Twitch-Alert-System-T4T/html/lines.txt', 'w+')
 f.write(str(num_lines) + '\n')
 f.close()
